78d0cd3d732330c0cd5bee66d16a23f2934d4f29
SamGombs20 <gombajosh18@gmail.com>
2026-02-10 14:34:52 +0300

    Update:Insertion sorting algorithm

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import lessons.sorting.Sorting;
 
 void main() {
     Sorting sort = new Sorting();
-    int [] arr = {3,2,1,10,9,7};
+    int [] arr = {8,4,2,1,10,9,7};
 //    sort.printList(arr);
     System.out.println(Arrays.toString(sort.insertionSort(arr)));
 }
--- src/lessons/sorting/Sorting.java
+++ src/lessons/sorting/Sorting.java
@@ -8,10 +8,11 @@ public class Sorting {
         for (int j=1;j<list.length;j++){
             key = list[j];
             i= j-1;
-            while (i>0 && list[i]>key){
+            while (i>=0 && list[i]>key){
                 list[i+1] = list[i];
                 i-=1;
             }
+
             list[i+1]=key;
         }
         return list;
